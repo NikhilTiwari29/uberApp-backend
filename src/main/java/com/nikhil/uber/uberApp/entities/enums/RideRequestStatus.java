14efa144076b3e5bf984b03bd36de7c5ec37660b
NikhilTiwari29 <NikhilTiwari29@users.noreply.github.com>
2026-05-23 14:52:05 +0530

    Initial commit

---
+++ src/main/java/com/nikhil/uber/uberApp/entities/enums/RideRequestStatus.java
@@ -0,0 +1,5 @@
+package com.nikhil.uber.uberApp.entities.enums;
+
+public enum RideRequestStatus {
+    PENDING, CANCELLED, CONFIRMED
+}
---
+++ src/main/java/com/nikhil/uber/uberApp/entities/enums/TransactionType.java
@@ -0,0 +1,5 @@
+package com.nikhil.uber.uberApp.entities.enums;
+
+public enum TransactionType {
+    CREDIT, DEBIT
+}
