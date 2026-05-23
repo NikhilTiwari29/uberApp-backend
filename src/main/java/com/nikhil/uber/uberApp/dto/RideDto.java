e8e4db4b6dbc5f8e0d858f24f1f87bb83e0944fa
NikhilTiwari29 <NikhilTiwari29@users.noreply.github.com>
2026-05-23 14:53:44 +0530

    Add API controllers and DTOs

---
+++ src/main/java/com/nikhil/uber/uberApp/dto/RideDto.java
@@ -0,0 +1,26 @@
+package com.nikhil.uber.uberApp.dto;
+
+import com.nikhil.project.uber.uberApp.entities.enums.PaymentMethod;
+import com.nikhil.project.uber.uberApp.entities.enums.RideStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class RideDto {
+
+    private Long id;
+    private PointDto pickupLocation;
+    private PointDto dropOffLocation;
+
+    private LocalDateTime createdTime;
+    private RiderDto rider;
+    private DriverDto driver;
+    private PaymentMethod paymentMethod;
+
+    private RideStatus rideStatus;
+
+    private Double fare;
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+}
