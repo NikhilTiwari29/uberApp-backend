9cd6ff7858c684820153cc60a3e11b6cf59d493d
NikhilTiwari29 <NikhilTiwari29@users.noreply.github.com>
2026-05-23 14:55:14 +0530

    Add repositories and JWT security

---
+++ src/main/java/com/nikhil/uber/uberApp/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.nikhil.uber.uberApp.repositories;
+
+import com.nikhil.project.uber.uberApp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
